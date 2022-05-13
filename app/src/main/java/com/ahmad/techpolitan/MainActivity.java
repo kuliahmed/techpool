1781b9264bc3eca6e4ef53b89e1929c0f5395306
Gandha Ryanto <gandharyanto@gmail.com>
2022-05-13 09:46:57 +0700

    Initial Project

---
+++ app/src/main/java/com/ahmad/techpolitan/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ahmad.techpolitan;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.page_1:
+                return true;
+            case R.id.page_2:
+                return true;
+            case R.id.page_3:
+                return true;
+            case R.id.page_4:
+                return true;
+            case R.id.page_5:
+                return true;
+        }
+        return false;
+    }
+}
